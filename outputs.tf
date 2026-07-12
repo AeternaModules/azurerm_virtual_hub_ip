7e0d28269f5989d324679d80f2bfc5d5f3d35355
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "virtual_hub_ips" {
-  description = "All virtual_hub_ip resources"
-  value       = azurerm_virtual_hub_ip.virtual_hub_ips
-}
 output "virtual_hub_ips_name" {
-  description = "List of name values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.name]
+  description = "Map of name values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.name }
 }
 output "virtual_hub_ips_private_ip_address" {
-  description = "List of private_ip_address values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.private_ip_address]
+  description = "Map of private_ip_address values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.private_ip_address }
 }
 output "virtual_hub_ips_private_ip_allocation_method" {
-  description = "List of private_ip_allocation_method values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.private_ip_allocation_method]
+  description = "Map of private_ip_allocation_method values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.private_ip_allocation_method }
 }
 output "virtual_hub_ips_public_ip_address_id" {
-  description = "List of public_ip_address_id values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.public_ip_address_id]
+  description = "Map of public_ip_address_id values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.public_ip_address_id }
 }
 output "virtual_hub_ips_subnet_id" {
-  description = "List of subnet_id values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.subnet_id]
+  description = "Map of subnet_id values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.subnet_id }
 }
 output "virtual_hub_ips_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all virtual_hub_ips"
-  value       = [for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all virtual_hub_ips, keyed the same as var.virtual_hub_ips"
+  value       = { for k, v in azurerm_virtual_hub_ip.virtual_hub_ips : k => v.virtual_hub_id }
 }
 
